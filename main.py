a4db74c88f639a3dfd8453fc91640737fc22983c
doplerer <carlosmarinperez1@gmail.com>
2024-11-25 23:59:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 # Artist to Music
 import api
+from concurrent.futures import ThreadPoolExecutor
+
+def controladorThreads(canciones, max_workers):
+    pass
+
+
 
 def main():
     cantante = input("Introduce el nombre del cantante: ")
     id_cantante = api.getId(cantante)
-    canciones = api.getCanciones(id_cantante)
-    
-    canciones_counter = 0
-    for cancion in canciones:
-        canciones_counter += 1
-        print(cancion.getTitle())
 
-    print( " ############## Se han mostrado: " + str(canciones_counter) + " canciones ##############")
+    canciones = api.getCanciones(id_cantante) # Nombres de canciones
+    links = [] # Links de descarga
+    
+    # Proceso de descargas
+    resultados = controladorThreads(canciones=canciones, max_workers=20)
+
+    canciones_counter = 0
+
+    for cancion in canciones:
+        canciones_counter+=1
+        print(cancion)
+
+
+    print( " Se han encotrado: " + str(canciones_counter) + " canciones")
     
 
 if __name__ == "__main__":
